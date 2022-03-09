4fff28059070b7d74242c1ed6aa5dcef7c7145da
Minh Nguyen <minh14496@gmail.com>
2022-03-08 20:16:23 -0800

    Update string.py

--- data_structure/string.py
+++ data_structure/string.py
@@ -1,4 +1,5 @@
-ord()
-chr()
-''.join()
-''.replace()
+ord('P') -> 80 # return the Unicode code from a given character
+chr(80) -> 'P' # return a character from an integer (represents unicode)
+','.join(['A', 'B']) -> 'A,B'
+str.replace(old, new[, max]) # returns a copy of the string in which the occurrences of old have been replaced with new
+# max If this optional argument max is given, only the first count occurrences are replaced
